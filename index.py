c812b841ff26439ef1def4b7fb7a9eee24754bbf
benny <littlebennyindahouse@gmail.com>
2026-04-07 17:41:58 +0200

    a

--- index.py
+++ index.py
@@ -1,14 +1,1 @@
-print('hello omg')
-
-a = [12,24,13,54,13,54,76,9]
-
-sum_element = 0
-
-for i in a:
-    sum_element+=i
-
-print(sum_element)
-print(sum_element/len(a))
-print('pipikiwipls:)')
-
-print('pipikivipls:)')
+print(5)
